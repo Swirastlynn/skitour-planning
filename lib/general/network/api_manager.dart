af7b113a944cdd8efc3e0c45169c9ebdc74e9442
Przemyslaw Lusnia <przemyslaw.lusnia@schibsted.com>
2021-11-19 14:03:00 +0100

    handling network exceptions for http client

---
+++ lib/general/network/api_manager.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+import 'custom_exceptions.dart';
+
+/// Manage API calls for simple http library.
+/// More sophisticated http library should support error handling with its own exceptions.
+class ApiManager {
+  Future<dynamic> getAPICall(Uri url) async {
+    print("Calling API: $url"); // todo logger
+
+    var jsonResponse;
+    try {
+      final httpResponse = await http.get(url);
+      jsonResponse = _validateResponse(httpResponse);
+    } on SocketException {
+      throw NoInternetConnectionException();
+    }
+    return jsonResponse;
+  }
+
+  dynamic _validateResponse(http.Response response) async {
+    var statusCode = response.statusCode;
+    if (statusCode >= 200 && statusCode < 300) {
+      final decodedJson = utf8.decode(response.bodyBytes);
+      return json.decode(decodedJson);
+    } else if (statusCode >= 300 && statusCode < 400) {
+      throw NotSupportedStatusCodeException(statusCode);
+    } else if (statusCode >= 400 && statusCode < 500) {
+      throw ApiException(statusCode);
+    } else if (statusCode >= 500) {
+      throw ServerException(statusCode);
+    }
+  }
+}
--- lib/people/people_list_screen.dart
+++ lib/people/people_list_screen.dart
@@ -1,37 +1,39 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'package:skitour_planning/general/network/api_manager.dart';
 import 'package:skitour_planning/people/post.dart';
 
 import 'details_screen.dart';
 
 class PeopleListScreen extends StatefulWidget {
-  PeopleListScreen({Key key}) : super(key: key);
+  PeopleListScreen({Key? key}) : super(key: key);
 
   @override
   _PeopleListScreenState createState() => _PeopleListScreenState();
 }
 
 class _PeopleListScreenState extends State<PeopleListScreen> {
+  final ApiManager apiManager = ApiManager();
   List<Post> posts = [];
 
   @override
   void initState() {
     super.initState();
-    loadData();
+    loadPosts();
   }
 
-  Future<void> loadData() async {
-    var dataURL = Uri.parse(
-        'https://jsonplaceholder.typicode.com/posts'); // todo handle exceptions
-    http.Response response = await http.get(dataURL);
-    setState(() {
-      var decodedJson = jsonDecode(utf8.decode(response.bodyBytes));
-      posts = decodedJson
-          .map<Post>((site) => Post.fromJson(site as Map<String, dynamic>))
-          .toList() as List<Post>;
-    });
+  Future<void> loadPosts() async {
+    try {
+      var jsonResponse = await apiManager
+          .getAPICall(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+      setState(() {
+        posts = jsonResponse
+            .map<Post>((site) => Post.fromJson(site))
+            .toList() as List<Post>;
+      });
+    } on Exception catch (error) {
+      print(
+          'Exception $error'); // there should be appropriate handling
+    }
   }
 
   @override
