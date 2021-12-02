4e80736dc97b3a3c735a3474935a29fa99e4474f
Przemyslaw Lusnia <przemyslaw.lusnia@schibsted.com>
2021-12-02 12:57:14 +0100

    move models to freezed ones

---
+++ lib/people/model/post.dart
@@ -0,0 +1,17 @@
+// main.dart
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:flutter/foundation.dart';
+
+part 'post.freezed.dart';
+part 'post.g.dart';
+
+@freezed
+class Post with _$Post {
+  factory Post(
+      int userId,
+      int id,
+      String title,
+      String body) = _Post;
+
+  factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
+}
